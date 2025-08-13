2f2026e9f75a6aec5db73c9df54d148710802168
Hamsa057 <24wh1a05aj@bvrithyderabad.edu.in>
2025-08-13 12:29:25 +0530

    StarPattern.java

---
+++ 24wh1a05aj/StarPattern.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Week1_StarPattern {
+
+	public static void main(String[] args) {
+		        Scanner sc=new Scanner(System.in);
+		        System.out.print("Enter n value:");
+		        int n=sc.nextInt();
+		        
+		        for (int i = 1; i <= n; i++)
+		        {
+		            for (int s = 1; s <= n - i; s++) 
+		            {
+		                System.out.print("  ");
+		            }
+		            for (int j = 1; j <= i; j++) 
+		            {
+		                System.out.print("*   ");
+		            }
+		            System.out.println();
+		        }
+		        
+		        
+		        for (int i = n - 1; i >= 1; i--)
+		        {
+		            for (int s = 1; s <= n - i; s++) 
+		            {
+		                System.out.print("  ");
+		            }  
+		            for (int j = 1; j <= i; j++) 
+		            {
+		                System.out.print("*   ");
+		            }
+		            System.out.println();
+		        }
+		        sc.close();
+   }
+}
+
+
