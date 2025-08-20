d930ddf28736f17ea3223c5c2a5c759b768d88c1
Charvi Parmar <24wh1a05bt@bvrithyderabad.edu.in>
2025-08-20 10:08:21 +0530

    Create TrianglePattern2.java

---
+++ 24WH1A05BT/TrianglePattern2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pattern2 {
+	private static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n-i; j++) {
+				System.out.print("  ");
+			}
+			for (int k = n-i; k < n; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
