def42ac31a564ba6e902735757b2323a0bb953d4
24wh1a05bq-pixel <24wh1a05bq@bvrithyderabad.edu.in>
2025-08-20 11:04:14 +0530

    Create triangleb.java

---
+++ 24wh1a05bq/triangleb.java
@@ -0,0 +1,21 @@
+package projects;
+
+public class triangleb {
+    public static void main(String[] args) {
+        int rows = 5; 
+
+        for (int i = 1; i <= rows; i++) {
+        
+            for (int j = rows; j > i; j--) {
+                System.out.print("  ");  
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println(); 
+        }
+    }
+}
+
+
+
