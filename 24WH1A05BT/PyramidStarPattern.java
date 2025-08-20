c0d0a5a806b8bbb4830b262f17c0dee07faf1888
Charvi Parmar <24wh1a05bt@bvrithyderabad.edu.in>
2025-08-20 10:22:38 +0530

    Create PyramidStarPattern.java

---
+++ 24WH1A05BT/PyramidStarPattern.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Pattern3 {
+	private static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < 2*i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+		for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k < 2*i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+	}
+
+}
