ef1eba6521a6c98821b6c1efff67beca44d15afd
24wh1a05ca-web <24wh1a05ca@bvrithyderabad.edu.in>
2025-08-20 11:06:38 +0530

    Create pattern4a.java

---
+++ 24WH1A05CA/pattern4a.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,n=6;
+		for(i=1;i<6;i++)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print(" * ");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
